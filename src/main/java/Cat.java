186f481a6eee5c2592bdf1d00c0ef5c3ec5e87ef
Андрей <PWNZAsuran@mail.ru>
2021-04-28 10:46:16 +0300

    Spring hello world

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String name;
+
+    public String getMessage() {
+        return name;
+    }
+
+    public void setMessage(String name) {
+        this.name = name;
+    }
+}
